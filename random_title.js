96a8abb60faa5913722d730fb017c435cec077dc
moomoohk <moomoohk@ymail.com>
2015-02-01 20:17:51 +0200

    Update random_title.js

--- random_title.js
+++ random_title.js
@@ -1,4 +1,4 @@
 function randomTitle() {
-  var titles = new Array("~~senpai jizz~~", "pingas is back on the menu boys", "kill me", "god bless the germans", "that's why they call me mr. autofellatio", "420 vape #it", "fear me", "pls halp", "whalecum 2 my site", "oh helleuuuu", "the henrog zone");
+  var titles = new Array("~~senpai jizz~~", "pingas is back on the menu boys", "kill me", "god bless the germans", "that's why they call me mr. autofellatio", "420 vape #it", "fear me", "pls halp", "whalecum 2 my site", "oh helleuuuu", "the henrog zone", "don't abort my fetus", "folow my twitter pleaes", "yes", "i am content", "peek-a-poo", "i make clay dildos");
   document.title = titles[parseInt(Math.random() * titles.length)];
 };
